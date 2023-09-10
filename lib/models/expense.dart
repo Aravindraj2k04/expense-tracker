d11ba71d431b5434fc668f284c04981f78042f54
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-10 23:06:07 +0530

    date formatted

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,19 +1,37 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
-const uuid=Uuid();
-enum Category {food ,entertainment,travel,shopping}
-class expense {
-  expense( 
-    {required this.title,
-     required this.amount, 
-     required this.date,
-     required this.category,
-     }) : id =uuid.v4();
+import 'package:intl/intl.dart';
 
+final formatting=DateFormat.yMd();
+
+const uuid = Uuid();
+
+enum Category { food, entertainment, travel, shopping }
+
+const category_icon = {
+  Category.food: Icons.fastfood,
+  Category.entertainment: Icons.movie,
+  Category.travel: Icons.flight,
+  Category.shopping: Icons.shopping_cart,
+};
+
+class expense {
+  expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
   final Category category;
-  
+
+  get  formattedDate{
+  return formatting.format(date);
 }
+
+}
+
