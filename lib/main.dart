5b2eaf23ff55f2b0b1170129767d5104e6f3c806
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-14 22:19:44 +0530

    title added

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ import 'expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      title: "Expense tracker",
+      //theme: ThemeData(useMaterial3: true),
+      title:('Expense tracker'),
       home: SafeArea(
         child: Scaffold(
           appBar:
               AppBar(actions: [
-                //backgroundcolor: Colors.red,
+                Title(
+                  color: Colors.blueAccent,
+                   child: Center(child: Text("Expense tracker")),
+                   ),
+                //backgroundColor: Colors.red,
                 IconButton(
                   onPressed: (){},
                    icon: Icon(Icons.add))]),
