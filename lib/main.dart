d0fbf5d049a58fdc06553a4ba7c8664ed46c17ab
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-14 21:24:25 +0530

    appbar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'expenses.dart';
 
-
-void main()
-{
-  runApp(MaterialApp(
-    title: "Expense tracker",
-    home: SafeArea(
-      child: 
-        Scaffold(
-          
-          body:expenses(),
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Expense tracker",
+      home: SafeArea(
+        child: Scaffold(
+          appBar:
+              AppBar(actions: [
+                ,
+                IconButton(
+                  onPressed: (){},
+                   icon: Icon(Icons.add))]),
+          body: expenses(),
         ),
-      
+      ),
     ),
-  ),
   );
 }
