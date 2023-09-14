544e4dde6caead9e6308c1df061c005eb375867e
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-14 21:29:47 +0530

    material3

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@ import 'expenses.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       title: "Expense tracker",
       home: SafeArea(
         child: Scaffold(
           appBar:
               AppBar(actions: [
-                ,
+                //backgroundcolor: Colors.red,
                 IconButton(
                   onPressed: (){},
                    icon: Icon(Icons.add))]),
