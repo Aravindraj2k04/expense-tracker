98d09974d31fe52750562a522b5f62f66f5850fc
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-09-26 14:43:43 +0530

    row added

--- lib/wigets/addnewExpense.dart
+++ lib/wigets/addnewExpense.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({Key? key}) : super(key: key);
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -9,14 +9,18 @@ class NewExpense extends StatefulWidget {
 
 var enteredtitle = '';
 
-void _saveTitleinput(String inputvalue) {
+void _saveTitleInput(String inputvalue) {
   enteredtitle = inputvalue;
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  final titlecontroller = TextEditingController();
+  final titleController = TextEditingController();
+  final enteredAmt = TextEditingController();
+
+  @override
   void dispose() {
-    titlecontroller.dispose(); 
+    titleController.dispose();
+    enteredAmt.dispose();
     super.dispose();
   }
 
@@ -24,23 +28,34 @@ class _NewExpenseState extends State<NewExpense> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
-      child: Column(children: [
-        TextField(
-          controller: titlecontroller,
-          maxLength: 50,
-          decoration: const InputDecoration(labelText: 'Title'),
-        ),
-        Row(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                print(titlecontroller.text);
-              },
-              child: Text('Save Expense'),
-            ),
-          ],
-        )
-      ]),
+      child: Column(
+        children: [
+          TextField(
+            controller: titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(labelText: 'Title'),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(titleController.text);
+                },
+                child: Text('Save Expense'),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              TextField(
+                controller: enteredAmt,
+                decoration: const InputDecoration(label: Text('Enter Amount')),
+                maxLength: 13,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
